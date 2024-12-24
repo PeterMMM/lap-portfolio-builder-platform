452a9550925bf88e08ccdac15befc081b3446f79
Lawm04 <yexlawmveung@gmail.com>
2024-12-24 11:21:25 +0630

    file

--- components/Login.js
+++ components/Login.js
@@ -1,16 +1,16 @@
 import React from "react";
 import Button from "./Button";
-
+import bgImage from "../images/bg.png";
 
 export default function Login() {
   return (
     <div
       className="flex flex-col flex-1 justify-center items-center gap-4 bg-cover bg-center min-h-screen"
       style={{
-        backgroundImage: "url('../images/bg.jpg')",
+        backgroundImage: `url(${bgImage.src})`, 
       }}
     >
-      <h4 className={" text-4xl sm:text-5xl md:text-6xl "}>Log In</h4>
+      <h4 className={" text-4xl text-white sm:text-5xl md:text-6xl "}>Login</h4>
       <input
         className="w-full max-w-[400px] mx-auto px-3 duration-200 hover:border-white-600 focus:border-white-600 py-2  sm:py-2 border border-solid border-white-400 rounded-full outline-none"
         placeholder="Email"
@@ -20,8 +20,8 @@ export default function Login() {
         placeholder="Password"
         type="password"
       />
-      <div className="max-w-[400px] w-full mx-auto flex justify-center items-center px-3  sm:py-2">
-        <Button text="Submit" full />
+      <div className="max-w-[400px] w-full mx-auto flex justify-center items-center px-3  sm:py-2 text-white">
+        <Button text="Log In" full />
       </div>
       <p className="text-center">
         Don't have an account? <span className="text-indigo-600">Sign in</span>
